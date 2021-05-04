22fc27bafd2aae96dd983b5209954c00a4526d1e
Alexandr <sashka.krup@gmail.com>
2021-05-04 20:10:16 +0300

    fix after review

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :category
-  has_many :questions
-  has_many :results
-  has_many :users, through: :results
+
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results, dependent: :destroy
 
   def self.titles_by_category(category)
-    joins('JOIN categories ON tests.category_id = categories.id')
+    joins(:category)
       .where(categories: { title: category })
       .order(title: :desc)
       .pluck(:title)
