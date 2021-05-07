764f39226e1aa4ba38d45bd93c1b3afa1f56a96f
Alexandr <sashka.krup@gmail.com>
2021-05-07 22:48:32 +0300

    done

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+
+  validate :validate_answers_count, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  def validate_answers_count
+    errors.add(:answers) if question.answers.size >= 4
+  end
 end
