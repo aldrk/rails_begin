96c90fed692c9dfa86aedfbc23992cbd8af3a3c1
Alexandr <sashka.krup@gmail.com>
2021-05-07 10:42:23 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
-  has_many :results, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :results
   has_many :tests, through: :results, dependent: :destroy
 
   def test_by_level(level = 0)
