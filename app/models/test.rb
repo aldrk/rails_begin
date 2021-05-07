959b309edadd81b52c2af0c12a0b9543850dca32
Alexandr <sashka.krup@gmail.com>
2021-05-07 10:31:42 +0300

    fix after review

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :category
 
-  has_many :questions, dependent: :destroy
-  has_many :results, dependent: :destroy
+  has_many :questions
+  has_many :results
   has_many :users, through: :results, dependent: :destroy
 
   def self.titles_by_category(category)
