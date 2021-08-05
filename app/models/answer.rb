da237d9d0fe1434cbf59801a7e343e93201c0023
Alexandr <sashka.krup@gmail.com>
2021-08-05 23:53:26 +0300

    fix validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,9 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+
+  validate :validate_answers_count, on: :create
+
+  scope :correct, -> { where(correct: true) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,17 @@ class Test < ApplicationRecord
   has_many :results
   has_many :users, through: :results, dependent: :destroy
 
-  def self.titles_by_category(category)
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
+  scope :titles_by_category, lambda { |category|
     joins(:category)
       .where(categories: { title: category })
       .order(title: :desc)
       .pluck(:title)
-  end
+  }
 end
