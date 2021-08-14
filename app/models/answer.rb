8631958a785df5242a86439e84239b728c962632
Alexandr <sashka.krup@gmail.com>
2021-08-14 21:46:06 +0300

    done answer CRUD

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,17 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :body, presence: true
-
+  validates :title, presence: true
   validate :validate_answers_count, on: :create
 
   scope :correct, -> { where(correct: true) }
+
+  attr_reader :answers
+
+  def validate_answers_count
+    puts @question, "QUESTION"
+
+
+    # errors.add(@answer, message: "can't be more the 4") if @question.answers.count >= 4
+  end
 end
