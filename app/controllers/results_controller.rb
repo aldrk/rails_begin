d9e993c0cbb0c2ed03478afdf7774fa4586d2ffd
Alexandr <sashka.krup@gmail.com>
2021-08-24 21:22:23 +0300

    finish video

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show; end
 
@@ -15,6 +15,18 @@ class ResultsController < ApplicationController
     else
       render :show
     end
+  end
+
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+
+    flash_options = if result.success?
+      { notice: t('.success') }
+    else
+      { notice: t('.failure') }
+    end
+
+    redirect_to @test_passage, flash_options
   end
 
   private
--- lib/clients/github_client.rb
+++ lib/clients/github_client.rb
@@ -1,13 +1,14 @@
 class GithubClient
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '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'
+  ACCESS_TOKEN = ''
 
   def initialize
     @http_client = setup_http_client
   end
 
   def create_gist(params)
-    @http_client.post('gists') do |request|
+    puts "AMDSOPJKDAOSPDOKASD", params
+    @http_client.post('/gists') do |request|
       request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
       request.headers['Content-Type'] = 'application/json'
       request.body = params.to_json
